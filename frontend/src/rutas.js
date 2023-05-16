931bc461bb5d7aaf6fc3ddce53e456f607567dac
Luis Goenaga <luis.goenaga@est.iudigital.edu.co>
2023-05-15 23:43:12 -0500

    Se depuraron vistas no utiliazdas

--- frontend/src/rutas.js
+++ frontend/src/rutas.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import ViewAdministracion from "./views/administacion";
+import ViewLogin from "./views/login";
 
 import ViewUsuarios from "./views/usuarios/usuarioslistar";
 import ViewCrearUsuario from "./views/usuarios/usuariocrear";
@@ -10,11 +11,6 @@ import ViewUpdateUsuario from "./views/usuarios/usuarioupdate";
 import ViewContactos from "./views/contactos/contactos";
 import ViewRegistroCiudadano from "./views/contactos/contactocrear";
 import ViewUpdateCiudadano from "./views/contactos/contactoupdate";
-
-import ViewLugarVotacion from "./views/lugarvotacion";
-import ViewAsistencia from "./views/asistencia";
-import ViewLogin from "./views/login";
-import ViewContactar from "./views/contactar";
 
 import ViewTestigoCrear from './views/testigos/testigocrear';
 import ViewTestigos from "./views/testigos/testigolistar";
@@ -29,9 +25,13 @@ const Rutas = () => {
   return (
 		<div className="container-app noMostrarRutas">
 			<Routes>
+
 				<Route path="*" element={<ViewLogin />} />
 				<Route exact path="/" element={<ViewLogin />} />
+				<Route exact path="/login" element={<ViewLogin />} />
+
 				<Route exact path="/inicio" element={<ViewAdministracion />} />
+
 				<Route exact path="/contactos" element={<ViewContactos />} />
 				<Route
 					exact
@@ -43,6 +43,7 @@ const Rutas = () => {
 					path="/contactos/:documentoId"
 					element={<ViewUpdateCiudadano />}
 				/>
+
 				<Route exact path="/usuarios" element={<ViewUsuarios />} />
 				<Route extact path="/usuarios/crear" element={<ViewCrearUsuario />} />
 				<Route
@@ -50,6 +51,7 @@ const Rutas = () => {
 					path="/usuarios/:userLogin"
 					element={<ViewUpdateUsuario />}
 				/>
+
 				<Route extact path="/testigos" element={<ViewTestigos />} />
 				<Route extact path="/testigos/crear" element={<ViewTestigoCrear />} />
 				<Route
@@ -57,14 +59,11 @@ const Rutas = () => {
 					path="/testigos/:documentoId"
 					element={<ViewUpdateTestigo />}
 				/>
+
 				<Route exact path="/lugares" element={<ViewLugares />} />
 				<Route exact path="/lugares/:id" element={<ViewUpdateLugar />} />
 				<Route exact path="/lugares/crear" element={<ViewCrearLugar />} />
-				
-				<Route exact path="/lugarvotacion" element={<ViewLugarVotacion />} />
-				<Route exact path="/Contactos/contactar" element={<ViewContactar />} />
-				<Route exact path="/asistencia" element={<ViewAsistencia />} />
-				<Route exact path="/login" element={<ViewLogin />} />
+
 			</Routes>
 		</div>
 	);
